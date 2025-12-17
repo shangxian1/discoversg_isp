fe16e6aeb46b9c0db32a577c033d74f2189ad27d
itznotsx <shangxian.lee@gmail.com>
2025-12-17 17:19:46 +0800

    updated backend for itinerary

--- discoversg-backend/server.js
+++ discoversg-backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const AIRoute = require('./AIroute');
+const AIRoute = require('./routes');
 
 app.use('/', AIRoute);
 
