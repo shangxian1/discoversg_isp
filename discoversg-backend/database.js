d4b285c78aa0696395e7e33c96cf25135ff605a1
YanKall <hoeyankai@gmail.com>
2026-01-21 15:23:06 +0800

    db con test

--- discoversg-backend/database.js
+++ discoversg-backend/database.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2/promise');
+/* const mysql = require('mysql2');
 
 // Configuration using the env variables loaded
 const dbConfig = {
@@ -9,8 +9,20 @@ const dbConfig = {
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-  
+
 };
+
+const db_string = 'mysql://doadmin:AVNS_kIVt5j17kdK1r8u3sPm@discoversgdb-do-user-32265640-0.j.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED';
+
+const connection = mysql.createConnection(db_string);
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database as id ' + connection.threadId);
+})
 
 if (!dbConfig.database) {
   throw new Error(
@@ -22,4 +34,23 @@ if (!global.db) {
   global.db = mysql.createPool(dbConfig);
 }
 
-module.exports = global.db;
+module.exports = global.db; */
+
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 25060,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: true
+  },
+  connectTimeout: 10000
+});
+
+module.exports = pool;
